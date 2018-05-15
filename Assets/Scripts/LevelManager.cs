e499e802de2339e4d96b9a07ec41523a32690835
Marko Jovanov <mkjovanov@gmail.com>
2018-05-15 20:45:59 +0200

    Initial starter project commit.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+	public class LevelManager : MonoBehaviour
+	{
+		public float LoadNextLevelAfter;
+
+		public void Start()
+		{
+			Invoke("LoadNextLevel", LoadNextLevelAfter);
+		}
+
+		public void LoadLevel(string name)
+		{
+			Debug.Log("New Level load: " + name);
+			SceneManager.LoadScene(name);
+		}
+
+		public void QuitRequest()
+		{
+			Debug.Log("Quit requested");
+			Application.Quit();
+		}
+
+		public void LoadNextLevel()
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
