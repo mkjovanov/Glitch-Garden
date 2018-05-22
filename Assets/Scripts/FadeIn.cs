8d7ceadab9492c1c4a5d701cff365ff503184862
Marko Jovanov <mkjovanov@gmail.com>
2018-05-22 23:24:17 +0200

    Main menu changes: - Added fade in panel for the starting main menu

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+	public class FadeIn : MonoBehaviour
+	{
+		public int FadeInTime;
+
+		private Image _fadePanel;
+		private Color _currentColor = Color.black;
+
+		private void Start()
+		{
+			_fadePanel = GetComponent<Image>();
+		}
+
+		private void Update()
+		{
+			if (Time.timeSinceLevelLoad < FadeInTime)
+			{
+				var alphaChange = Time.deltaTime / FadeInTime;
+				_currentColor.a -= alphaChange;
+				_fadePanel.color = _currentColor;
+			}
+			else
+			{
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
