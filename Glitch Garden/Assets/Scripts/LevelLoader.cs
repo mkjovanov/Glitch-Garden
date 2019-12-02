dea0169d133697226db2a88b0b41ab0982a9aef3
Marko Jovanov <mkjovanov@gmail.com>
2019-12-02 21:23:38 +0100

    Added splash and start game scenes

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+	[SerializeField] Object SceneToLoad;
+	[SerializeField] int TimeToWait = 3;
+    
+    IEnumerator Start()
+    {
+		yield return new WaitForSeconds(TimeToWait);
+		SceneManager.LoadScene(SceneToLoad.name);
+    }
+}
